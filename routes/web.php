e8c37eb54a35e8e12636d1d131dc848476c69cd2
HardikGopi4533 <gopinath@rsoft.in>
2026-02-25 16:23:27 +0530

    check file save cmit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
+Route::get('/r2-test', function () {
+    Storage::disk('r2')->put('r2-test1.txt', 'R2 is not working');
+    return 'R2 connection successful';
+});
 Route::get('/', function () {
     return view('welcome');
 });
